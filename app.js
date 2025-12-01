b82a766b92158826796a49e874a0ac99b13b4c8f
Vicky Kumar Kushwaha <vickykumarkusgwaha@gmail.com>
2025-12-01 13:23:12 +0530

    root added

--- app.js
+++ app.js
@@ -84,6 +84,10 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 app.all("*", (req, res, next) => {
     next(new expressError(404, "Page not found!"));
 });
